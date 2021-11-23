2223cc636c1d1fd4bbbf076b4531fb571b5e62d3
Mohammadali Setork <60846538+mohammadalisetork@users.noreply.github.com>
2021-11-23 19:19:41 +0330

    Add files via upload

---
+++ Jpeg.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+
+printf("salam");#include <stdio.h>
+
+int main()
+{
+
+
+
+
+
+return 0;
+printf("salam";#include <stdio.h>
+
+int main()
+{
+
+
+
+
+
+return 0;
+printf("salam";)
+})
+}
+
+
+
+
+return 0;
+}
